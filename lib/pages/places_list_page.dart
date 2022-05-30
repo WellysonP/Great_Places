c16216cdf0518b13f46410f069d03d9765aea28a
Wellyson Pontes <wellysonpontes@gmail.com>
2022-05-30 17:44:52 -0300

    adicionado sqflite para persistir os dados... Estudar sobre SQL

--- lib/pages/places_list_page.dart
+++ lib/pages/places_list_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:great_places/providers/great_places.dart';
 import 'package:great_places/utils/app_routes.dart';
+import 'package:provider/provider.dart';
 
 class PlacesListPage extends StatelessWidget {
   const PlacesListPage({Key? key}) : super(key: key);
@@ -7,18 +9,41 @@ class PlacesListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Meus Lugares"),
-          actions: [
-            IconButton(
-              onPressed: () =>
-                  Navigator.of(context).pushNamed(AppRoutes.PLACE_FORM),
-              icon: const Icon(Icons.add),
-            )
-          ],
-        ),
-        body: const Center(
-          child: CircularProgressIndicator(),
-        ));
+      appBar: AppBar(
+        title: const Text("Meus Lugares"),
+        actions: [
+          IconButton(
+            onPressed: () =>
+                Navigator.of(context).pushNamed(AppRoutes.PLACE_FORM),
+            icon: const Icon(Icons.add),
+          )
+        ],
+      ),
+      body: FutureBuilder(
+        future: Provider.of<GreatPlaces>(context, listen: false).loadPlaces(),
+        builder: (ctx, snapshot) => snapshot.connectionState ==
+                ConnectionState.waiting
+            ? Center(child: CircularProgressIndicator())
+            : Consumer<GreatPlaces>(
+                child: Center(
+                  child: Text("Nenhum local cadastrado!"),
+                ),
+                builder: (ctx, greatPlaces, child) =>
+                    greatPlaces.itemsCount == 0
+                        ? child!
+                        : ListView.builder(
+                            itemCount: greatPlaces.itemsCount,
+                            itemBuilder: (ctx, i) => ListTile(
+                              leading: CircleAvatar(
+                                backgroundImage:
+                                    FileImage(greatPlaces.itemByIndex(i).image),
+                              ),
+                              title: Text(greatPlaces.itemByIndex(i).title),
+                              onTap: () {},
+                            ),
+                          ),
+              ),
+      ),
+    );
   }
 }
---
+++ lib/utils/db_util.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/cupertino.dart';
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:path/path.dart' as path;
+
+class DbUtil {
+  static Future<sql.Database> dataBase() async {
+    final dbPath = await sql.getDatabasesPath();
+    return sql.openDatabase(
+      path.join(dbPath, "places.db"),
+      onCreate: (db, version) {
+        return db.execute(
+            "CREATE TABLE places (id TEXT PRIMARY KEY, title TEXT, image TEXT)");
+      },
+      version: 1,
+    );
+  }
+
+  static Future<void> insert(String table, Map<String, Object> data) async {
+    final db = await DbUtil.dataBase();
+    await db.insert(
+      table,
+      data,
+      conflictAlgorithm: sql.ConflictAlgorithm.replace,
+    );
+  }
+
+  static Future<List<Map<String, dynamic>>> getData(String table) async {
+    final db = await DbUtil.dataBase();
+    return db.query(table);
+  }
+}
